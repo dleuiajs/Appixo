09f8f0ccc087617824f8c0c0182c4445408ba760
dleuiajs <jduejdb322@gmail.com>
2026-01-08 13:33:28 +0100

    Pridaná zmena témy v navigácii

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,9 +1,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useTheme } from 'vuetify';
 
 export default defineComponent(
     {
-        name: "TheNavigation"
+        name: "TheNavigation",
+        setup() {
+            {
+                const theme = useTheme();
+
+                return { theme };
+            }
+        },
     }
 )
 </script>
@@ -15,6 +23,8 @@ export default defineComponent(
             <v-btn to="/" text="Home"></v-btn>
             <v-btn to="/apps/games" text="Games"></v-btn>
             <v-btn to="/apps/software" text="Software"></v-btn>
+            <v-divider inset vertical></v-divider>
+            <v-btn icon="mdi-theme-light-dark" @click="theme.toggle()"></v-btn>
         </v-toolbar-items>
     </v-toolbar>
 </template>
