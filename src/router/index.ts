d0390c2877a079c51cb9eb8a1f3bc90a657c6086
dleuiajs <jduejdb322@gmail.com>
2026-01-12 13:21:57 +0100

    Teraz sa pri prechode medzi stránkami pozícia posúva vynuluje

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,6 +27,13 @@ const router = createRouter({
       component: () => import('@/views/NotFound.vue')
     }
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  }
 })
 
 export default router
