e7fb3b31929b237ef7445905149e33a35f294608
dleuiajs <jduejdb322@gmail.com>
2026-01-17 22:02:47 +0100

    Odkazy prepracované z /Appixo

--- src/utils/getImgUrl.ts
+++ src/utils/getImgUrl.ts
@@ -1,3 +1,3 @@
 export function getImgUrl(imgUrlFromJson: string): string {
-    return new URL('../assets/images/' + imgUrlFromJson, import.meta.url).href;
+    return new URL('/Appixo/images/' + imgUrlFromJson, import.meta.url).href;
 }
